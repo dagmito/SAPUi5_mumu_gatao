8ed61dc631d1dd171bb23783ef31fd70a086616c
Dagmito <36495952+dagmito@users.noreply.github.com>
2018-04-09 20:36:47 -0300

    commit 8

---
+++ Aula 4/controller/Page1.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+    "use strict";
+    return Controller.extend("treinamento.ui5.controller.App", {
+
+        onInit: function () {},
+
+        onBeforeRendering: function () {},
+
+        onAfterRendering: function () {},
+
+        onExit: function () {},
+
+        onPress: function () {
+
+        },
+    });
+});
