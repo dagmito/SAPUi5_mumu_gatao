a87c1b83a0be0e392d144d2b18055982de7e4435
Dagmito <36495952+dagmito@users.noreply.github.com>
2018-04-09 19:45:30 -0300

    commit 3

---
+++ Aula 4/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    'sap/ui/core/UIComponent'
+], function (UIComponent) {
+    'use strict';
+
+    return UIComponent.extend("treinamento.ui5.Component", {
+
+        init: function(){
+            UIComponent.prototype.init.apply(this, arguments);
+        }
+    });
+});
