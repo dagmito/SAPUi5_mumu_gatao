b9c5e6307afe0553bfba54aa32a123c53d88ee72
Dagmito <36495952+dagmito@users.noreply.github.com>
2018-04-10 08:26:28 -0300

    commit

---
+++ Desafio Estacionamento 2/controller/Page1.controller.js
@@ -0,0 +1,21 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+    "use strict";
+    return Controller.extend("treinamento.ui5.controller.Page1", {
+
+        onInit: function () {},
+
+        onBeforeRendering: function () {},
+
+        onAfterRendering: function () {},
+
+        onExit: function () {},
+
+        onPress: function () {
+            this.getOwnerComponent().getRouter().navTo("Page2",{
+                value: this.byId("input").getValue()
+            });
+        }
+    });
+});
