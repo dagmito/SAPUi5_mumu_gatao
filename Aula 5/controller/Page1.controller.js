890d7136550e53d3a37f83c8d358d0ca8a6b4ea4
Dagmito <36495952+dagmito@users.noreply.github.com>
2018-04-10 21:17:00 -0300

    commit

--- Aula 5/controller/Page1.controller.js
+++ Aula 5/controller/Page1.controller.js
@@ -1,21 +1,44 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/resource/ResourceModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (Controller, ResourceModel, Filter, FO) {
     "use strict";
     return Controller.extend("treinamento.ui5.controller.Page1", {
 
-        onInit: function () {},
-
-        onBeforeRendering: function () {},
-
-        onAfterRendering: function () {},
-
-        onExit: function () {},
-
         onPress: function () {
-            this.getOwnerComponent().getRouter().navTo("Page2",{
+            this.getOwnerComponent().getRouter().navTo("Page2", {
                 value: this.byId("input").getValue()
             });
+        },
+
+        onTranslate: function (bilola) {
+
+            var i18n = this.getOwnerComponent().getModel("i18n");
+
+            var lang = bilola.getParameter("state");
+
+            if  (lang) {
+                var i18nModel = new ResourceModel({
+                    bundleName: "treinamento.ui5.i18n.i18n"
+                });
+            } else {
+                var i18nModel = new ResourceModel({
+                    bundleName: "treinamento.ui5.i18n.i18n_enUS"
+                });
+            }
+
+            this.getOwnerComponent().setModel("i18n", i18nModel);
+        },
+
+        onSearch: function(oEvent){
+            var query = oEvent.getParameter("query");
+            var filter = new Filter("ProductName", FO.Contains, query);
+
+            this.byId("table").getBinding("items").filter(filter);
         }
+
     });
+
 });
